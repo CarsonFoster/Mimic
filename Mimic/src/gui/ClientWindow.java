41784d4d9cf33b212697a5e801272c869bfbe2b3
CarsonFoster <carsonwfoster@gmail.com>
2020-02-04 13:06:47 -0600

    Work done in class 2/4/2020. Worked on GUI some and mostly researched swing classes.

--- Mimic/src/gui/ClientWindow.java
+++ Mimic/src/gui/ClientWindow.java
@@ -3,18 +3,29 @@ package gui;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Toolkit;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 public class ClientWindow extends JFrame implements Client{
 
-    private int X, Y;
+    private static int WIDTH, HEIGHT;
+    /*private Painter p;
+    
+    private interface Painter {
+        public void paint(Graphics g);
+    }
     
     private class ClientPanel extends JPanel {
         public void paintComponent(Graphics g) {
-            g.fillRect(0, 0, 50, 50);
+            p.paint(g);
         }
+    }*/
+    
+    public int getWidth() {
+        return WIDTH;
+    } 
+    
+    public int getHeight() {
+        return HEIGHT;
     }
     
     @Override
@@ -29,22 +40,42 @@ public class ClientWindow extends JFrame implements Client{
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
+    private static JScrollPane constructChannelsList() {
+        String[] abcs = new String[52];
+        for (int i = 0; i < 26; i++) {
+            abcs[i*2] = "" + (char)(97 + i);
+            abcs[i*2 + 1] = "" + (char)(65 + i);
+        }
+        JList<String> list = new JList<>(abcs);
+        JScrollPane scroll = new JScrollPane(list);
+        scroll.setPreferredSize(new Dimension(40, HEIGHT));
+        return scroll;
+    }
+    
+    private static JTextField constructMessages() {
+        JTextField a = new JTextField();
+        
+        return a;
+    }
+    
     public ClientWindow() {
-        super("SCI Graphing");
+        super("Mimic");
         Dimension screensize = Toolkit.getDefaultToolkit().getScreenSize();
-        X = (int)(screensize.getWidth() * 3.0/7.0);
-        Y = (int)(screensize.getHeight() * 2.0/3.0);
-        setPreferredSize(new Dimension(X, Y));
+        WIDTH = (int)(screensize.getWidth() * 3.0/7.0);
+        HEIGHT = (int)(screensize.getHeight() * 2.0/3.0);
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        add(new ClientPanel());
+        //add(new ClientPanel());
+        
+        add(ClientWindow.constructChannelsList());
         pack();
         setVisible(true);
     }
     
     public static void main(String[] args) {
         ClientWindow cw = new ClientWindow();
-        cw.promptForUsername();
-        cw.errorUsername();
+        //cw.promptForUsername();
+        //cw.errorUsername();
     }
 }
