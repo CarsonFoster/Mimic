d3b897fcdbf860db0265396c50e2bb25030b39c5
CarsonFoster <carsonwfoster@gmail.com>
2020-02-06 13:05:16 -0600

    Work done in class 2/6/2020. Worked on adding things to the GUI in the right places using a GridBagLayout.

--- Mimic/src/gui/ClientWindow.java
+++ Mimic/src/gui/ClientWindow.java
@@ -1,8 +1,7 @@
 package gui;
 
 import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Toolkit;
+import java.awt.*;
 import javax.swing.*;
 
 public class ClientWindow extends JFrame implements Client{
@@ -40,7 +39,7 @@ public class ClientWindow extends JFrame implements Client{
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
-    private static JScrollPane constructChannelsList() {
+    private static void constructChannelList(Container pane) {
         String[] abcs = new String[52];
         for (int i = 0; i < 26; i++) {
             abcs[i*2] = "" + (char)(97 + i);
@@ -48,14 +47,43 @@ public class ClientWindow extends JFrame implements Client{
         }
         JList<String> list = new JList<>(abcs);
         JScrollPane scroll = new JScrollPane(list);
-        scroll.setPreferredSize(new Dimension(40, HEIGHT));
-        return scroll;
+        scroll.setMaximumSize(new Dimension(WIDTH / 4, HEIGHT)); //TODO: fix this; it doesn't work, but also not high priority
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 0; c.gridy = 1;
+        c.gridwidth = 1; c.gridheight = GridBagConstraints.REMAINDER;
+        c.weighty = 1.0;
+        c.fill = GridBagConstraints.VERTICAL;
+        //JPanel wrapper = new JPanel(null);
+        //wrapper.setLayout(new BoxLayout(wrapper, BoxLayout.LINE_AXIS));
+        //wrapper.add(scroll);
+        pane.add(scroll, c);
     }
     
-    private static JTextField constructMessages() {
+    private static void constructMessages(Container pane) {
+        JTextArea a = new JTextArea();
+        a.setEditable(false);
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 1; c.gridy = 1;
+        c.gridwidth = GridBagConstraints.REMAINDER; c.gridheight = GridBagConstraints.RELATIVE;
+        c.weightx = 1; c.weighty = 1;
+        c.fill = GridBagConstraints.BOTH;
+        pane.add(a, c);
+    }
+    
+    private static void constructYourMessage(Container pane) {
         JTextField a = new JTextField();
-        
-        return a;
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 1; c.gridy = 2;
+        c.gridwidth = GridBagConstraints.RELATIVE; c.gridheight = GridBagConstraints.RELATIVE;
+        c.weightx = 1; c.weighty = 0;
+        c.fill = GridBagConstraints.BOTH;
+        pane.add(a, c);
+        JButton b = new JButton("Send");
+        c.gridx = 2; c.gridy = 2;
+        c.gridwidth = GridBagConstraints.REMAINDER; c.gridheight = GridBagConstraints.RELATIVE;
+        c.weightx = 0; c.weighty = 0;
+        c.fill = GridBagConstraints.BOTH;
+        pane.add(b, c);
     }
     
     public ClientWindow() {
@@ -65,10 +93,12 @@ public class ClientWindow extends JFrame implements Client{
         HEIGHT = (int)(screensize.getHeight() * 2.0/3.0);
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-        //add(new ClientPanel());
-        
-        add(ClientWindow.constructChannelsList());
+        //setLocationRelativeTo(null);
+        Container pane = getContentPane();
+        pane.setLayout(new GridBagLayout());
+        constructChannelList(pane);
+        constructMessages(pane);
+        constructYourMessage(pane);
         pack();
         setVisible(true);
     }
